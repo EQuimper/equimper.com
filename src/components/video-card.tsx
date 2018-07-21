4a64c1e539ac59a8f65871c29dc5ee69c51493c5
Emanuel Quimper <quimperemanuel@gmail.com>
2018-07-21 14:46:57 -0400

    Abstract video card

---
+++ src/components/video-card.tsx
@@ -0,0 +1,67 @@
+import Img from 'gatsby-image'
+import React from 'react'
+
+import styled from '../utils/styled'
+
+const Container = styled('div')`
+  ${tw('w-full w-full flex')};
+`
+
+const Wrapper = styled('div')`
+  ${tw('flex flex-1 flex-col items-center bg-white rounded shadow mb-8')};
+`
+
+const VideoTitle = styled('h2')`
+  ${tw('text-base text-grey-darkest font-bold')};
+`
+
+const ImgWrapper = styled('div')`
+  ${tw('flex-1')};
+`
+
+const DetailWrapper = styled('div')`
+  ${tw('w-2/3 pl-4')};
+`
+
+const VideoDate = styled('p')`
+  ${tw('text-grey-dark text-sm')};
+`
+
+interface IProps {
+  data: {
+    id: string
+    title: string
+    description: string
+    thumbnail: {
+      url: string
+      height: number
+      width: number
+    }
+    channelTitle: string
+    publishedAt: string
+    localThumbnail: {
+      childImageSharp: {
+        resolutions: any
+      }
+    }
+  }
+}
+
+const VideoCard = ({ data }: IProps) => (
+  <Container>
+    <Wrapper>
+      <ImgWrapper>
+        <Img
+          alt="thumbnail"
+          resolutions={data.localThumbnail.childImageSharp.resolutions}
+        />
+      </ImgWrapper>
+      <DetailWrapper>
+        <VideoTitle>{data.title}</VideoTitle>
+        <VideoDate>Published at: {data.publishedAt}</VideoDate>
+      </DetailWrapper>
+    </Wrapper>
+  </Container>
+)
+
+export default VideoCard
