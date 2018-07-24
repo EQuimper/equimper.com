ae5372f2e9388fb434b482faedc62fc9d563015b
Emanuel Quimper <quimperemanuel@gmail.com>
2018-07-24 01:14:56 -0400

    Fix issue with window on deploy

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,39 +1,41 @@
 /* tslint:disable no-console */
 
-export const clearStorage = () => localStorage.clear()
+const localStorageGlobal = typeof window !== 'undefined' && window.localStorage
+
+export const clearStorage = () => localStorageGlobal && localStorageGlobal.clear()
 
 export const getItemFromStorage = (key: string) => {
-  if (!localStorage) {
+  if (!localStorageGlobal) {
     return
   }
 
   try {
     // @ts-ignore
-    return JSON.parse(localStorage.getItem(key))
+    return JSON.parse(localStorageGlobal.getItem(key))
   } catch (err) {
     console.error(`Error getting item ${key} from localStoragee`, err)
   }
 }
 
 export const storeItem = (key: string, item: any) => {
-  if (!localStorage) {
+  if (!localStorageGlobal) {
     return
   }
 
   try {
-    return localStorage.setItem(key, JSON.stringify(item))
+    return localStorageGlobal.setItem(key, JSON.stringify(item))
   } catch (err) {
     console.error(`Error storing item ${key} to localStoragee`, err)
   }
 }
 
 export const removeItemFromStorage = (key: string) => {
-  if (!localStorage) {
+  if (!localStorageGlobal) {
     return
   }
 
   try {
-    return localStorage.removeItem(key)
+    return localStorageGlobal.removeItem(key)
   } catch (err) {
     console.error(`Error removing item ${key} from localStoragee`, err)
   }
