7bd8b98ab95b4baf1d989154f419f2dbf4e82f1e
Emanuel Quimper <quimperemanuel@gmail.com>
2018-07-28 17:21:55 -0400

    Remove unused css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -34,6 +34,7 @@ injectGlobal`
     --grey-dark: #8795a1;
     --grey-darkest: #3d4852;
     --grey-lighter: #f3f7f9;
+    --green: #38c172;
   }
 
   body {
@@ -191,7 +192,7 @@ injectGlobal`
 
   .control:hover input:not([disabled]):checked ~ .control__indicator,
   .control input:checked:focus ~ .control__indicator {
-    background: #38c172;
+    background: var(--green);
   }
 
   .control input:disabled ~ .control__indicator {
@@ -308,79 +309,3 @@ const Layout: SFC = ({ children }) => (
 )
 
 export default Layout
-
-// /* Checkbox */
-//   /* [type="checkbox"] label {
-//     display: inline-block;
-//     position: relative;
-//     vertical-align: middle;
-//     padding-left: 5px;
-//   } */
-
-//   [type="checkbox"]:not(:checked),
-//   [type="checkbox"]:checked {
-//     position: absolute;
-//     left: -9999px;
-//   }
-//   [type="checkbox"]:not(:checked) + label,
-//   [type="checkbox"]:checked + label {
-//     position: relative;
-//     padding-left: 1.95em;
-//     cursor: pointer;
-//   }
-
-//   [type="checkbox"]:not(:checked) + label:before,
-//   [type="checkbox"]:checked + label:before {
-//     content: '';
-//     position: absolute;
-//     left: 0; top: 0;
-//     width: 1.25em; height: 1.25em;
-//     border: 2px solid #ccc;
-//     background: #fff;
-//     border-radius: 4px;
-//     box-shadow: inset 0 1px 3px rgba(0,0,0,.1);
-//   }
-//   /* checked mark aspect */
-//   [type="checkbox"]:not(:checked) + label:after,
-//   [type="checkbox"]:checked + label:after {
-//     content: '✔';
-//     position: absolute;
-//     top: .2em; left: .275em;
-//     font-size: 1.4em;
-//     line-height: 0.8;
-//     color: #38c172;
-//     transition: all .2s;
-//     font-family: Helvetica, Arial, sans-serif;
-//   }
-//   /* checked mark aspect changes */
-//   [type="checkbox"]:not(:checked) + label:after {
-//     opacity: 0;
-//     transform: scale(0);
-//   }
-//   [type="checkbox"]:checked + label:after {
-//     opacity: 1;
-//     transform: scale(1);
-//   }
-//   /* disabled checkbox */
-//   [type="checkbox"]:disabled:not(:checked) + label:before,
-//   [type="checkbox"]:disabled:checked + label:before {
-//     box-shadow: none;
-//     border-color: #bbb;
-//     background-color: #ddd;
-//   }
-//   [type="checkbox"]:disabled:checked + label:after {
-//     color: #999;
-//   }
-//   [type="checkbox"]:disabled + label {
-//     color: #aaa;
-//   }
-//   /* accessibility */
-//   [type="checkbox"]:checked:focus + label:before,
-//   [type="checkbox"]:not(:checked):focus + label:before {
-//     border: 2px dotted var(--grey-dark);
-//   }
-
-//   /* hover style just for information */
-//   label:hover:before {
-//     border: 2px solid var(--grey-darkest)!important;
-//   }
