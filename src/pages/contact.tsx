ac2330af5646698b7c961816808306fc9d7019cb
Emanuel Quimper <quimperemanuel@gmail.com>
2018-07-31 22:03:17 -0400

    Update contact content + add pic box

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import React, { SFC } from 'react'
 
 import siteConfig from '../../data/siteConfig'
 import ContactForm from '../components/contact-form'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import SocialFollow from '../components/social-follow'
 import styled from '../utils/styled'
 
 const Root = styled('div')`
@@ -11,7 +14,17 @@ const Root = styled('div')`
 `
 
 const Wrapper = styled('div')`
-  ${tw('bg-white rounded shadow p-4')};
+  ${tw('bg-white rounded shadow p-4 mb-8')};
+`
+
+const AvatarWrapper = styled('div')`
+  ${tw('mr-4 mb-4 sm:mb-0')};
+`
+
+const BottomWrapper = styled(Wrapper)`
+  ${tw(
+    'flex flex-col sm:flex-row items-center text-center sm:text-left sm:justify-start'
+  )};
 `
 
 const TopWrapper = styled('div')`
@@ -19,14 +32,26 @@ const TopWrapper = styled('div')`
 `
 
 const Title = styled('h1')`
-  ${tw('leading-tight tracking-wide text-grey-darker font-bold text-2xl mt-0')};
+  ${tw('tracking-wide text-grey-darker font-bold text-2xl mt-0')};
 `
 
 const Message = styled('p')`
   ${tw('leading-tight tracking-small text-grey-darker')};
 `
 
-const Contact = () => (
+const Avatar = styled(Img)`
+  ${tw('rounded-full')};
+`
+
+interface IProps {
+  data: {
+    avatarImg: {
+      fixed: any
+    }
+  }
+}
+
+const Contact: SFC<IProps> = ({ data }) => (
   <Layout>
     <SEO url={`${siteConfig.site.url}/contact`} customTitle="Contact Me" />
 
@@ -38,14 +63,34 @@ const Contact = () => (
           <Message>
             You want to connect with me about anything? You can just wrote a
             message here with your name, email and subject. I would answer you
-            when as fast as possible.
+            as fast as possible.
           </Message>
         </TopWrapper>
 
         <ContactForm />
       </Wrapper>
+
+      <BottomWrapper>
+        <AvatarWrapper>
+          <Avatar fixed={data.avatarImg.fixed} />
+        </AvatarWrapper>
+        <div>
+          <Title>You can also follow or contact me on</Title>
+          <SocialFollow />
+        </div>
+      </BottomWrapper>
     </Root>
   </Layout>
 )
 
 export default Contact
+
+export const query = graphql`
+  query ContactQuery {
+    avatarImg: imageSharp(original: { src: { regex: "/avatar/" } }) {
+      fixed(width: 100) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`
