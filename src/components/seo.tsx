8e62abf3620f19c277c40a1dbe8badd26a4c55fa
Emanuel Quimper <quimperemanuel@gmail.com>
2018-11-29 10:50:05 -0500

    Create a custom 404 page

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -83,7 +83,6 @@ const SEO = ({ postMeta, url, isBlogPost, postImage, customTitle }: IProps) => {
       <meta name="twitter:image" content={image} />
 
       {/* Google Verification tags */}
-
       <meta name="google-site-verification" content={siteConfig.googleVerification} />
 
       <title>{title}</title>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,48 @@
 import React, { SFC } from 'react'
 
+import NotFound from '../components/icons/not-found'
 import Layout from '../components/layout'
+import styled from '../utils/styled'
+
+const Wrapper = styled('div')`
+  ${tw('flex flex-col lg:flex-row align-center justify-center')}
+`
+
+const TextWrapper = styled('div')`
+  ${tw('flex flex-1 text-center lg:text-left align-center justify-center')};
+
+  h1 {
+    ${tw('tracking-wide text-grey-darker font-bold')};
+  }
+
+  p {
+    ${tw('leading-tight tracking-small text-grey-darker')};
+  }
+`
+
+const IconWrapper = styled('div')`
+  ${tw('flex flex-1')};
+`
+
+const NotFoundIcon = styled(NotFound)`
+  ${tw('text-blue-dark')};
+
+  width: 90%;
+`
 
 const NotFoundPage: SFC = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Wrapper>
+      <IconWrapper>
+        <NotFoundIcon />
+      </IconWrapper>
+      <TextWrapper>
+        <div>
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </TextWrapper>
+    </Wrapper>
   </Layout>
 )
 
