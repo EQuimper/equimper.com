9c84c32f248ba63638ce80873e9eae24a877c7f7
Emanuel Quimper <quimperemanuel@gmail.com>
2019-02-05 10:30:51 -0500

    add message when ew update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,14 @@ exports.onRouteUpdate = () => {
     }
   }, 200)
 }
+
+exports.onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
